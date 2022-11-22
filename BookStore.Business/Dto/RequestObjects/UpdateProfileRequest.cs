d5f6ac3b7eecdc6faa5a1f7dca191af5123b8d0a
quoclinh1201 <quoclinh1212000@gmail.com>
2022-11-23 00:20:40 +0700

    commit

--- BookStore.Business/Dto/RequestObjects/UpdateProfileRequest.cs
+++ BookStore.Business/Dto/RequestObjects/UpdateProfileRequest.cs
@@ -9,20 +9,17 @@ namespace BookStore.Business.Dto.RequestObjects
 {
     public class UpdateProfileRequest
     {
-        [Required]
         [MaxLength(50)]
         public string? FullName { get; set; }
         
         [MaxLength(50)]
         public string? Email { get; set; }
 
-        [Required]
         [RegularExpression(@"[0]{1}[0-9]{9}")]
         [MaxLength(10)]
         [MinLength(10)]
         public string? PhoneNumber { get; set; }
 
-        [Required]
         public bool Gender { get; set; }
     }
 }
