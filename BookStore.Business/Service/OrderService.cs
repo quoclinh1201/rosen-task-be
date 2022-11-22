41c76221818c0cdf85e0eae43c7983c9c6f41755
quoclinh1201 <quoclinh1212000@gmail.com>
2022-11-22 17:26:19 +0700

    update format money

--- BookStore.Business/Service/OrderService.cs
+++ BookStore.Business/Service/OrderService.cs
@@ -155,7 +155,7 @@ namespace BookStore.Business.Service
                         ProductId = item.ProductId,
                         ProductName = item.Product.ProductName,
                         Quantity = item.Quantity,
-                        Price = price.ToString().Substring(0, price.ToString().Length - 4),
+                        Price = FormatMoney.FormatPrice(price),
                         ProductImageUrl = image.ImageUrl
                     };
                     listItems.Add(i);
