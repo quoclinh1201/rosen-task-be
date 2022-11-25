8fc3a9e9f61303b30d3d18005373bd559c84fb9c
quoclinh1201 <quoclinh1212000@gmail.com>
2022-11-25 15:20:41 +0700

    update result

--- BookStore.Business/Helpers/Common/Result.cs
+++ BookStore.Business/Helpers/Common/Result.cs
@@ -11,6 +11,5 @@ namespace BookStore.Business.Helpers.Common
         public T Content { get; set; }
         public Error Error { get; set; }
         public bool IsSuccess => Error == null;
-        public DateTime ResponseTime { get; set; } = DateTime.UtcNow.AddHours(7);
     }
 }
